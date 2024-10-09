1ac58f1ac5761a78f53032d9014ae2b87bc5cfb5
dattavelivela <withgurudatta@gmail.com>
2024-10-09 23:07:59 +0530

    comm

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,6 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
+import { defineConfig } from 'vite';
+
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
@@ -11,6 +13,15 @@ const config = {
   framework: {
     name: "@storybook/react-vite",
     options: {},
+  },
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  viteFinal: async (config) => {
+    return defineConfig({
+      ...config,
+      base: './', // Set the base to './' to ensure relative paths work
+    });
   },
 };
 export default config;
